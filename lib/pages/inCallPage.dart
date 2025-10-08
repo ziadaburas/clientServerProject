b0c318e741a489ca8ec6729d643e854886bc921c
ZiadAburas <ziad2018zzzz@gmail.com>
2025-10-08 17:57:32 +0300

    online 3

--- lib/pages/inCallPage.dart
+++ lib/pages/inCallPage.dart
@@ -157,7 +157,30 @@ class _InCallPageState extends State<InCallPage> with WidgetsBindingObserver {
     return {
       'iceServers': [
         {'urls': 'stun:stun.l.google.com:19302'},
-        {'urls': 'stun:stun1.l.google.com:19302'},
+        {
+        'urls': "stun:stun.relay.metered.ca:80",
+      },
+      {
+        'urls': "turn:global.relay.metered.ca:80",
+        'username': "c29dcbe114770d516fa6bcc3",
+        'credential': "c9jG2yn1tatk/tiw",
+      },
+      {
+        'urls': "turn:global.relay.metered.ca:80?transport=tcp",
+        'username': "c29dcbe114770d516fa6bcc3",
+        'credential': "c9jG2yn1tatk/tiw",
+      },
+      {
+        'urls': "turn:global.relay.metered.ca:443",
+        'username': "c29dcbe114770d516fa6bcc3",
+        'credential': "c9jG2yn1tatk/tiw",
+      },
+      {
+        'urls': "turns:global.relay.metered.ca:443?transport=tcp",
+        'username': "c29dcbe114770d516fa6bcc3",
+        'credential': "c9jG2yn1tatk/tiw",
+      },
+
         // يمكن إضافة خوادم TURN هنا للشبكات المعقدة
       ],
       'iceCandidatePoolSize': 10,
